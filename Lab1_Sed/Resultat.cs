5ce4b802d2fe759df9eba9348caf80a366def130
Juria1005 <shami1987@yandex.ru>
2022-06-15 15:35:48 +0200

    Добавьте файлы проекта.

---
+++ Lab1_Sed/Resultat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Sed
+{
+    internal class Resultat
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public Resultat(double a, double b)
+        {
+            A = a; B = b; 
+        }
+        public double Resh()
+        {
+            return A * A+ B * B;    
+    }
+    }
+    
+
+}
